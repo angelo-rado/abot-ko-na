69eb28dc014aeb6e7d56b0531f11b06fee241168
Angelo Paulo Rado <angelo.rado0295@gmail.com>
2025-08-08 03:36:05 +0800

    Patch Alpha: bug fix

---
+++ src/app/login/_components/LoginWithRedirect.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import { auth, provider } from '@/lib/firebase'
+import { signInWithPopup } from 'firebase/auth'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { Button } from '@/components/ui/button'
+import { useEffect } from 'react'
+import { useAuth } from '@/lib/useAuth'
+
+export function LoginWithRedirect() {
+  const router = useRouter()
+  const searchParams = useSearchParams()
+  const redirectPath = searchParams.get('redirect') ?? '/'
+  const { user } = useAuth()
+
+  useEffect(() => {
+    if (user) {
+      router.replace(redirectPath)
+    }
+  }, [user, redirectPath])
+
+  const handleLogin = async () => {
+    try {
+      await signInWithPopup(auth, provider)
+      router.replace(redirectPath)
+    } catch (err) {
+      console.error('Login failed', err)
+    }
+  }
+
+  return (
+    <main className="h-screen flex items-center justify-center">
+      <Button onClick={handleLogin}>Sign in with Google</Button>
+    </main>
+  )
+}
