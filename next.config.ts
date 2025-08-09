ecf07e25250d230f185057099393a456f9d0d492
Angelo Paulo Rado <angelo.rado0295@gmail.com>
2025-08-09 16:14:55 +0800

    Patch Alpha: bug fixes

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,8 @@
 const withPWA = require('next-pwa')({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
-  register: true,
+  register: false,
   skipWaiting: true,
-  swSrc: 'firebase-messaging-sw.js', 
 })
 
 /** @type {import('next').NextConfig} */
