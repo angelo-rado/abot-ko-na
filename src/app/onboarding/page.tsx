5edbcabb75ec38f28e270386e3b2e6ac73bb2497
Angelo Paulo Rado <angelo.rado0295@gmail.com>
2025-08-12 02:23:58 +0800

    Patch alpha: bug fixes

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -1,15 +1,12 @@
 'use client'
 
 import { Suspense } from 'react'
-import Providers from '@/app/providers'
 import OnboardingPage from './_OnBoardingPage' // your existing component
 
 export default function Page() {
   return (
-    <Providers>
       <Suspense fallback={<div className="p-6 text-center">Loading...</div>}>
         <OnboardingPage />
       </Suspense>
-    </Providers>
   )
 }
